27a1cf9c46cf548b98b62913eafb2b24ac6586e9
KzjLe <tomi95sz@gmail.com>
2020-01-24 12:53:56 +0100

    Created demo app.

---
+++ build/precache-manifest.170238c8feb2d198daebfdc43ab9549d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ecaf5b74e760221947a186f7be94a0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e6ab82db1a6a96c662c",
+    "url": "/static/js/2.e0c7ecbb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e0c7ecbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5219210ff88580f34fb",
+    "url": "/static/js/main.ff38d277.chunk.js"
+  },
+  {
+    "revision": "895b1a06739ab1682423",
+    "url": "/static/js/runtime-main.03df21c0.js"
+  }
+]);
